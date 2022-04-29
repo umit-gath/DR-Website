35b8d563c920a9681c12c408db572fac16c77dd5
umit-gath <umit.gath@gmail.com>
2022-04-29 05:15:01 +0200

    Back To Top

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,32 @@
 
 
 $(window).scroll(function() {
+
     var y = $(this).scrollTop();
 
     if (y > 150) {
         $('.navbar').removeClass("AnimateTrans");
         $('.navbar').addClass("AnimateBlack");
+
+        $('#BackToTop').removeClass("hideBTT");
+        $('#BackToTop').addClass("showBTT");
+        
     } else {
         $('.navbar').removeClass("AnimateBlack");
         $('.navbar').addClass("AnimateTrans");
+        
+        $('#BackToTop').removeClass("showBTT");
+        $('#BackToTop').addClass("hideBTT");
+        
     }
+  
+
+
+});
+
+
+$("#BackToTop").click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    },1000);
   });
